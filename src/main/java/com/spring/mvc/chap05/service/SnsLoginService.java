1a1694dac01a95c40684c041e32fa36fef84c691
mipi1256 <mipi1258@gmail.com>
2024-04-09 13:23:42 +0900

    #38. 카카오 로그인 신청 및 인가코드 받기

---
+++ src/main/java/com/spring/mvc/chap05/service/SnsLoginService.java
@@ -0,0 +1,67 @@
+package com.spring.mvc.chap05.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SnsLoginService {
+
+
+   public void kakaoLogin(Map<String, String> params) {
+
+
+
+   }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
